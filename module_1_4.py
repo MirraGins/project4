636ca23b5ae6ec08bb0f98e8d743af294d945fcf
MirraGins <mirra1@me.com>
2024-09-20 17:14:04 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+my_string = input ("what language are you learning? ")
+count = len(my_string)
+print(my_string)
+print(count)
+print(my_string.lower())
+print(my_string.upper())
+print("what language are you learning? ".replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
